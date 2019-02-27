aa9d3a28f34e15ce106fb2b365bbed177b462dd2
Vladan Krstic <vlkrst@gmail.com>
2019-02-27 22:01:19 +0100

    Removed unnecessary stuff to make the project a true stub.

---
+++ src/tdd_stub.h
@@ -0,0 +1,9 @@
+#ifndef TDD_STUB_H
+#define TDD_STUB_H
+
+class MyClass
+{
+
+};
+
+#endif // TDD_STUB_H
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,25 +1,17 @@
 #include "gtest/gtest.h"
-#include "game.h"
 
 namespace
 {
 
-class BowlingGameTest: public testing::Test
+TEST(MyTestCase, EmptyTest)
 {
-  void SetUp() override
-  {
 
-  }
-};
-
-TEST_F(BowlingGameTest, GutterGame)
-{
-  Game g;
 }
 
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
